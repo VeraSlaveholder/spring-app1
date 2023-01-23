ff14e149356d52e726093f719b95d902a507e857
VeraSlaveholder <bulgakova.vera2003@mail.ru>
2023-01-23 03:15:23 +0300

    start of the application(5)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -2,10 +2,32 @@ package org.example;
 
 public class MusicPlayer {
     private Music music;
+    private  String name;
+    private  String volume;
 
-    public MusicPlayer(Music music){
-        this.music=music;
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public MusicPlayer(Music music){this.music=music;}
+    public MusicPlayer(){}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic(){
         System.out.println("Playing "+music.getSong());
     }
